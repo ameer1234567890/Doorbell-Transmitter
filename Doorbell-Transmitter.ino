fb8624990724dac74b76304717f7e47e0d534033
Ameer Dawood <ameer1234567890@gmail.com>
2019-08-29 15:04:00 +0500

    Ring only if system is armed

--- Doorbell-Transmitter.ino
+++ Doorbell-Transmitter.ino
@@ -8,6 +8,8 @@
 #define WIFI_SSID "xxxxxxxxxx"
 #define WIFI_PASSWORD "xxxxxxxxxx"
 #define THINGSPEAK_KEY "xxxxxxxxxx"
+#define APILIO_VARIABLE "variable"
+#define APILIO_KEY "xxxxxxxxxxxxxxxxxxxxxx"
 String URLS[] = {
                   "http://maker.ifttt.com/trigger/doorbell/with/key/xxxxxxxxxxxxxxxxxxxxxx",
                   "http://lab.grapeot.me/ifttt/delay?event={IFTTT_EVENT_NAME}&t={TIME_IN_MINUTES}&key={IFTTT_KEY}"
@@ -40,43 +42,48 @@ void setup() {
     ArduinoOTA.begin();
   }
   pinMode(LED_PIN, OUTPUT);
-  batVoltage = ESP.getVcc();
   setupWifi();
   if (!manualBoot) {
-    int numUrls = sizeof(URLS) / sizeof(URLS[0]);
-    for (int i = 0; i < numUrls; i++) {
-      bool httpSucceeded = false;
-      for (int j = 0; j < 5; j++) {
-        Serial.print("Requesting URL: ");
-        Serial.print(URLS[i]);
-        Serial.print(" - Status: ");
-        if (postHTTP(URLS[i])) {
-          httpSucceeded = true;
-          break;
+    if (armed()) {
+      Serial.println("System is armed! Continuing...");
+      int numUrls = sizeof(URLS) / sizeof(URLS[0]);
+      for (int i = 0; i < numUrls; i++) {
+        bool httpSucceeded = false;
+        for (int j = 0; j < 5; j++) {
+          Serial.print("Requesting URL: ");
+          Serial.print(URLS[i]);
+          Serial.print(" - Status: ");
+          if (postHTTP(URLS[i])) {
+            httpSucceeded = true;
+            break;
+          }
+          delay(2000); // wait 2 seconds before we retry
         }
-        delay(2000); // wait 2 seconds before we retry
+        if (httpSucceeded) {
+          Serial.println("HTTP Succeeded!");
+          digitalWrite(LED_PIN, HIGH);
+          delay(1000);
+          digitalWrite(LED_PIN, LOW);
+        } else {
+          Serial.println("HTTP Failed!");
+          digitalWrite(LED_PIN, HIGH);
+          delay(200);
+          digitalWrite(LED_PIN, LOW);
+          delay(200);
+          digitalWrite(LED_PIN, HIGH);
+          delay(200);
+          digitalWrite(LED_PIN, LOW);
+          delay(200);
+          digitalWrite(LED_PIN, HIGH);
+          delay(200);
+          digitalWrite(LED_PIN, LOW);
+        }
       }
-      if (httpSucceeded) {
-        Serial.println("HTTP Succeeded!");
-        digitalWrite(LED_PIN, HIGH);
-        delay(1000);
-        digitalWrite(LED_PIN, LOW);
-      } else {
-        Serial.println("HTTP Failed!");
-        digitalWrite(LED_PIN, HIGH);
-        delay(200);
-        digitalWrite(LED_PIN, LOW);
-        delay(200);
-        digitalWrite(LED_PIN, HIGH);
-        delay(200);
-        digitalWrite(LED_PIN, LOW);
-        delay(200);
-        digitalWrite(LED_PIN, HIGH);
-        delay(200);
-        digitalWrite(LED_PIN, LOW);
-      }
+    } else {
+      Serial.println("System is not armed! Exiting...");
     }
     Serial.print("Sending battery voltage to ThingSpeak... ");
+    batVoltage = ESP.getVcc();
     postToThingSpeak();
     delay(1000);
     digitalWrite(LED_PIN, LOW);
@@ -117,6 +124,38 @@ void postToThingSpeak() {
   uint httpCode = http.GET();
   Serial.println(httpCode);
   http.end();
+}
+
+
+bool armed() {
+  Serial.print("Checking if Apilio status is true... Status: ");
+  String auth = "Basic " + String(APILIO_KEY);
+  String url = "/api/v1/boolean_variables/" + String(APILIO_VARIABLE);
+  wClientSecure.setInsecure(); // until we have better handling of a trust chain on small devices
+  http.begin(wClientSecure, "api.apilio.com", 443, url, true);
+  http.addHeader("Accept", "application/json");
+  http.addHeader("Authorization", auth);
+  uint httpCode = http.GET();
+  String httpResponse = http.getString();
+  Serial.println(httpCode);
+  http.end();
+  if (httpCode == 200) {
+    int startIndex = httpResponse.indexOf("value") + 7;
+    int endIndex = httpResponse.indexOf("\"", startIndex) - 1;
+    String status = httpResponse.substring(startIndex, endIndex);
+    if (status == "true") {
+      return true;
+    } else if (status == "false") {
+      return false;
+    } else {
+      Serial.println("Returning true status due to invalid HTTP response!"); // graceful degradation
+      Serial.println(httpResponse);
+      return true;
+    }
+  } else {
+    Serial.println("Returning true status due to HTTP error!"); // graceful degradation
+    return true;
+  }
 }
 
 
